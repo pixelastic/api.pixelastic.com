8ae4204b40bbd603c142b30a95592f0a17963cbe
Tim Carry <tim@pixelastic.com>
2021-05-11 22:59:32 +0200

    Add more tests

--- functions/screenshots/__tests__/index.js
+++ functions/screenshots/__tests__/index.js
@@ -1,19 +1,34 @@
 const got = require('golgoth/got');
 
 describe.integration('/screenshots/', () => {
-  it('should take a screenshot', async () => {
-    const targetUrl =
-      'http://localhost:8888/__tests__/screenshots/?query=stiff&sort=asc#anchor';
-
-    const suffix = targetUrl.replace('://', '/');
-    const url = `http://localhost:8888/screenshots/${suffix}`;
+  const serverUrl = 'http://localhost:8888';
+  it.each([
+    [
+      'Nominal case',
+      `${serverUrl}/screenshots/`,
+      `${serverUrl}/__tests__/screenshots/`,
+    ],
+    [
+      'With query string',
+      `${serverUrl}/screenshots/`,
+      `${serverUrl}/__tests__/screenshots/?query=stuff&sort=asc`,
+    ],
+    [
+      'Revved file',
+      `${serverUrl}/screenshots/revv:abcdef01/`,
+      `${serverUrl}/__tests__/screenshots/`,
+    ],
+  ])('%s', async (_title, apiUrl, targetUrl) => {
+    const suffixUrl = targetUrl.replace('://', '/');
+    const url = `${apiUrl}${suffixUrl}`;
     const actual = await got(url, {
       responseType: 'buffer',
     });
 
     const errorMessage = dedent`
+      apiUrl:    ${apiUrl}
       targetUrl: ${targetUrl}
-      url: ${url}
+      url:       ${url}
     `;
 
     expect(actual.rawBody, errorMessage).toMatchImageSnapshot();
